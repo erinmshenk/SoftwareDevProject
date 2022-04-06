80ce98704fea9c5827ca9af2b2dc565d8dd15358
Erin Shenk <eshenk@cs.hartford.edu>
2022-04-06 18:40:15 -0400

    Added main method

--- src/main/java/EmergencyRoom.java
+++ src/main/java/EmergencyRoom.java
@@ -2,46 +2,49 @@ import java.util.Scanner;
 
 public class EmergencyRoom {
     
-    Scanner keyboard = new Scanner(System.in);
-    
-    String username = keyboard.nextLine();
-    keyboard.nextLine();
-    
-    String password = keyboard.nextLine();
-    
-    //usernames for all parties that can log in
-    String nuser = "nurse";
-    String ruser = "registrar";
-    String puser = "physician";
-    String buser = "billing";
-    
-    //passwords for all parties that can log in
-    String npass = "123!";
-    String rpass = "456!";
-    String ppass = "789!";
-    String bpass = "010!";
-    
-    if (username.equals(nuser) && password.equals(npass))
+    public static void main(String[] args)
     {
-        
-    }
-    
-    else if (username.equals(ruser) && password.equals(rpass))
-    {
-        
-    }
-    
-    else if (username.equals(puser) && password.equals(ppass))
-    {
-        
-    }
-    
-    else if (username.equals(buser) && password.equals(bpass))
-    {
-        
-    }
-    
-    else
-    {
-        exit(0);
+        Scanner keyboard = new Scanner(System.in);
+
+        String username = keyboard.nextLine();
+        keyboard.nextLine();
+
+        String password = keyboard.nextLine();
+
+        //usernames for all parties that can log in
+        String nuser = "nurse";
+        String ruser = "registrar";
+        String puser = "physician";
+        String buser = "billing";
+
+        //passwords for all parties that can log in
+        String npass = "123!";
+        String rpass = "456!";
+        String ppass = "789!";
+        String bpass = "010!";
+
+        if (username.equals(nuser) && password.equals(npass))
+        {
+
+        }
+
+        else if (username.equals(ruser) && password.equals(rpass))
+        {
+
+        }
+
+        else if (username.equals(puser) && password.equals(ppass))
+        {
+
+        }
+
+        else if (username.equals(buser) && password.equals(bpass))
+        {
+
+        }
+
+        else
+        {
+            exit(0);
+        }
     }
