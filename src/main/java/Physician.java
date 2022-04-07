fde1b5b9d6424e9613088b09918aeba2da2f2e57
Branden267 <60358292+Branden267@users.noreply.github.com>
2022-04-07 10:58:11 -0400

    Added String statement for variables

--- src/main/java/Physician.java
+++ src/main/java/Physician.java
@@ -1,11 +1,24 @@
 public class Physician extends EmergencyRoom{
     
+    String tests;
+    String observation;
+    String symptoms;
+    String discharge;
+    String medications;
+    String diagnoses;
     
-    
-    
-    public Physician(){
+    public Physician(String tests, String observation, String symptoms, String discharge,
+    String medications, String diagnoses){
         
-        super();
+        super(tests, observation, symptoms, discharge, medications, diagnoses);
         
     }
+    
+    public String toString()
+    {
+        return (super.toString()+"The tests required by the physician: "+ tests +", doctors observation notes: "
+                + observation +", symptoms noticed: "+ symptoms +", discharge instructions: "+ discharge +
+                ", prescribed medications: "+ medications +", final diagnoses: "+ diagnoses);
+    }
+}
 }
