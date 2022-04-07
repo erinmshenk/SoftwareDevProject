8b73991d3a51ecfff217aed29cace84350f43937
Branden267 <60358292+Branden267@users.noreply.github.com>
2022-04-07 10:58:26 -0400

    Added String statement for variables

--- src/main/java/Nurse.java
+++ src/main/java/Nurse.java
@@ -1,18 +1,28 @@
 public class Nurse extends EmergencyRoom{
     
     int vitals;
-    int bloodpressure;
-    int height;
+    String height;
     int weight;
     int nightsstayed;
+    String bloodpressure;
     String admittance;
     String observation;
     String pretreatment;
     
-    public Nurse(int vitals, int bloodpressure, int height, int weight, int nightsstayed,
+    public Nurse(int vitals, String height, int weight, int nightsstayed, String bloodpressure,
     String admittance, String observation, String pretreatment){
         
-        super(vitals, bloodpressure, height, weight, nightsstayed, admittance, observation, pretreatment);
+        super(vitals, height, weight, nightsstayed, bloodpressure, admittance, observation, pretreatment);
         
     }
+    
+    public String toString()
+    {
+        return (super.toString()+"patient's vitals: "+ vitals +", the patient's height is: "+ height +
+                ", the patient's weight is: "+ weight +"lbs., the patient has stayed "+ nightsstayed +
+                "nights in the emergency room, the patient's blood pressure: "+ bloodpressure +
+                "does the patient require admittance? "+ admittance +"my notes on the patient's condition: "+ observation +
+                ", the pre-treatment performed to the patient before seeing the doctor: "+ pretreatment);
+    }
+}
 }
