132c77d69e81ba968588772d4e2e7527e244722b
Branden267 <60358292+Branden267@users.noreply.github.com>
2022-04-07 10:23:21 -0400

    Added inheritance to Nurse

--- src/main/java/Nurse.java
+++ src/main/java/Nurse.java
@@ -1,4 +1,18 @@
-
-public class Nurse {
-    int Erin;
+public class Nurse extends EmergencyRoom{
+    
+    int vitals;
+    int bloodpressure;
+    int height;
+    int weight;
+    int nightsstayed;
+    String admittance;
+    String observation;
+    String pretreatment;
+    
+    public Nurse(int vitals, int bloodpressure, int height, int weight, int nightsstayed,
+    String admittance, String observation, String pretreatment){
+        
+        super(vitals, bloodpressure, height, weight, nightsstayed, admittance, observation, pretreatment);
+        
+    }
 }
