61e49c5019c50890b6a8da29235d9c9718a9b5a7
Erin Shenk <eshenk@cs.hartford.edu>
2022-04-11 17:23:53 -0400

    user input to search for patient

--- src/main/java/Billing.java
+++ src/main/java/Billing.java
@@ -1,3 +1,14 @@
-public class Billing extends EmergencyRoom{
+import java.util.Scanner;
+
+public class Billing extends EmergencyRoom
+{
+    Scanner keyboard = new Scanner(System.in);
     
+    //user input to search for patient
+    String nameFirst = keyboard.nextLine();
+    String nameLast = keyboard.nextLine();
+    String ssn = keyboard.nextLine();
+    
+            
+            
 }
