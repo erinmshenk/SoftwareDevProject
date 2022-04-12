e7b6dc32c911123420af891b89d5ea7618a43a19
Erin Shenk <eshenk@cs.hartford.edu>
2022-04-11 21:54:30 -0400

    insert statement for patient

--- src/main/java/Registrar.java
+++ src/main/java/Registrar.java
@@ -1,3 +1,9 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -29,8 +35,24 @@ public class Registrar
     String medcon;
     boolean alcdrug;
     
-    //send info to database
-    
-    
-    
+    //should be called when sumbit button is pushed by registrar
+    public void insertPatient()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:8888/hospitalSystem", "root", "root");
+                Statement stmt = con.createStatement();
+                //insert patient
+                stmt.executeUpdate("INSERT INTO `hospitalSystem`.`Patient`\n" + "(`patientID`,\n" + "`firstName`,\n" + 
+                        "`lastName`,\n" + "`dob`,\n" + "`address`,\n" + "`zip`,\n" + "`ssn`,\n" + "`insurance`,\n" + 
+                        "`primaryPhys`,\n" + "`height`,\n" + "`weight`,\n" + "`vax1`,\n" + "`vax2`,\n" + "`symptom`,\n" + 
+                        "`allergy`,\n" + "`meds`,\n" + "`alcdrug`)\n" + 
+                        "VALUES\n" + "(AUTO_INCREMENT,\n" + nameFirst + ",\n" + nameLast +",\n" + dob + ",\n" +
+                         address + ",\n" + zip + ",\n" + ssn + ",\n" + insurance + ",\n" + phys + ",\n" + height + ",\n" +
+                         weight + ",\n" + vax1 + ",\n" + vax2 + ",\n" + symptom + ",\n" + allergy + ",\n" +
+                         meds + ",\n" + alcdrug + ");");
+        }catch(ClassNotFoundException | SQLException e){System.out.println(e);}
+    }
+
 }
