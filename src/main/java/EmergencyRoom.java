5ed9df2efe932fc71bae0e92e6c7c7c1f7c1afd7
Erin Shenk <eshenk@cs.hartford.edu>
2022-04-11 21:17:40 -0400

    create and connect database

--- src/main/java/EmergencyRoom.java
+++ src/main/java/EmergencyRoom.java
@@ -12,7 +12,136 @@ public class EmergencyRoom {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:8888/hospitalSystem", "root", "root");
             Statement stmt = con.createStatement();
-        }catch(Exception e){System.out.println(e);}
+            
+            //creates the database *only run this section once*
+            /**
+            stmt.executeUpdate("CREATE DATABASE hospitalSystem");
+            stmt.executeUpdate("""
+                               CREATE TABLE `Patient` (
+                                 `patientID` int(11) NOT NULL AUTO_INCREMENT,
+                                 `firstName` varchar(45) DEFAULT NULL,
+                                 `lastName` varchar(45) DEFAULT NULL,
+                                 `dob` varchar(45) DEFAULT NULL,
+                                 `address` varchar(45) DEFAULT NULL,
+                                 `zip` varchar(45) DEFAULT NULL,
+                                 `ssn` varchar(45) DEFAULT NULL,
+                                 `insurance` varchar(45) DEFAULT NULL,
+                                 `primaryPhys` varchar(45) DEFAULT NULL,
+                                 `height` int(11) DEFAULT NULL,
+                                 `weight` int(11) DEFAULT NULL,
+                                 `vax1` varchar(45) DEFAULT NULL,
+                                 `vax2` varchar(45) DEFAULT NULL,
+                                 `symptom` varchar(150) DEFAULT NULL,
+                                 `allergy` varchar(45) DEFAULT NULL,
+                                 `meds` varchar(45) DEFAULT NULL,
+                                 `alcdrug` varchar(45) DEFAULT NULL,
+                                 PRIMARY KEY (`patientID`)
+                               )""");
+            stmt.executeUpdate("""
+                               CREATE TABLE `tests` (
+                                 `testID` int(11) NOT NULL AUTO_INCREMENT,
+                                 `testName` varchar(45) DEFAULT NULL,
+                                 `price` int(11) DEFAULT NULL,
+                                 PRIMARY KEY (`testID`)
+                               ) """);
+            stmt.executeUpdate("""
+                               CREATE TABLE `nursePhysicianRecord` (
+                                 `patientID` int(11) NOT NULL,
+                                 `vitals` varchar(45) DEFAULT NULL,
+                                 `height` int(11) DEFAULT NULL,
+                                 `weight` int(11) DEFAULT NULL,
+                                 `nightsStayed` int(11) DEFAULT NULL,
+                                 `bloodpressure` varchar(45) DEFAULT NULL,
+                                 `admitted` varchar(45) DEFAULT NULL,
+                                 `observation` varchar(45) DEFAULT NULL,
+                                 `pretreatment` varchar(45) DEFAULT NULL,
+                                 `testID` int(11) DEFAULT NULL,
+                                 `symptoms` varchar(45) DEFAULT NULL,
+                                 `discharge` varchar(45) DEFAULT NULL,
+                                 `medication` varchar(45) DEFAULT NULL,
+                                 `diagnosis` varchar(45) DEFAULT NULL,
+                                 PRIMARY KEY (`patientID`),
+                                 KEY `testID` (`testID`),
+                                 CONSTRAINT `patientID` FOREIGN KEY (`patientID`) REFERENCES `Patient` (`patientID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+                                 CONSTRAINT `testID` FOREIGN KEY (`testID`) REFERENCES `tests` (`testID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+                               ) """);
+            stmt.executeUpdate("""
+                               CREATE TABLE `diagnosis` (
+                                 `conditionID` int(11) NOT NULL AUTO_INCREMENT,
+                                 `condition` varchar(45) DEFAULT NULL,
+                                 `medication` varchar(45) DEFAULT NULL,
+                                 `price` int(11) DEFAULT NULL,
+                                 PRIMARY KEY (`conditionID`)
+                               ) """);
+            stmt.executeUpdate("""
+                               INSERT INTO `hospitalSystem`.`diagnosis`
+                               (`conditionID`,
+                               `condition`,
+                               `medication`,
+                               `price`)
+                               VALUES
+                               (AUTO_INCREMENT,
+                               'diabetes',
+                               'insulin',
+                               100),
+                               (AUTO_INCREMENT,
+                               'heart disease',
+                               'pravastatin',
+                               200),
+                               (AUTO_INCREMENT,
+                               'high blood pressure',
+                               'asprin',
+                               50),
+                               (AUTO_INCREMENT,
+                               'acid reflux',
+                               'omeprazole',
+                               50),
+                               (AUTO_INCREMENT,
+                               'pulled muscle',
+                               'tramadol',
+                               300),
+                               (AUTO_INCREMENT,
+                               'ibs',
+                               'miralax',
+                               150);""");
+            stmt.executeUpdate("""
+                               INSERT INTO `hospitalSystem`.`tests`
+                               (`testID`,
+                               `testName`,
+                               `price`)
+                               VALUES
+                               (AUTO_INCREMENT,
+                               'red blood cell',
+                               200),
+                               (AUTO_INCREMENT,
+                               'white blood cell',
+                               200),
+                               (AUTO_INCREMENT,
+                               'liver function',
+                               300),
+                               (AUTO_INCREMENT,
+                               'renal function',
+                               450),
+                               (AUTO_INCREMENT,
+                               'electrolyte',
+                               150),
+                               (AUTO_INCREMENT,
+                               'x ray',
+                               350),
+                               (AUTO_INCREMENT,
+                               'computed tomography',
+                               575),
+                               (AUTO_INCREMENT,
+                               'magnetic resonance image',
+                               600),
+                               (AUTO_INCREMENT,
+                               'urinary test',
+                               200),
+                               (AUTO_INCREMENT,
+                               'stool test',
+                               175);""");
+            **/
+        }catch(ClassNotFoundException | SQLException e){System.out.println(e);}
         
         Scanner keyboard = new Scanner(System.in);
 
