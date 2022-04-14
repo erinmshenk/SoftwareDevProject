104f22306bcdf163f833e7e08565acc7f7aa5985
Branden267 <60358292+Branden267@users.noreply.github.com>
2022-04-13 20:56:28 -0400

    Imported necessary packages

--- src/main/java/Nurse.java
+++ src/main/java/Nurse.java
@@ -1,28 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Nurse extends EmergencyRoom{
     
-    int vitals;
-    String height;
-    int weight;
-    int nightsstayed;
-    String bloodpressure;
-    String admittance;
-    String observation;
-    String pretreatment;
+    Scanner keyboard = new Scanner(System.in);
     
-    public Nurse(int vitals, String height, int weight, int nightsstayed, String bloodpressure,
-    String admittance, String observation, String pretreatment){
-        
-        super(vitals, height, weight, nightsstayed, bloodpressure, admittance, observation, pretreatment);
-        
-    }
-    
-    public String toString()
-    {
-        return (super.toString()+"patient's vitals: "+ vitals +", the patient's height is: "+ height +
-                ", the patient's weight is: "+ weight +"lbs., the patient has stayed "+ nightsstayed +
-                "nights in the emergency room, the patient's blood pressure: "+ bloodpressure +
-                "does the patient require admittance? "+ admittance +"my notes on the patient's condition: "+ observation +
-                ", the pre-treatment performed to the patient before seeing the doctor: "+ pretreatment);
-    }
-}
+    //user input to search for patient
+    String nameFirst = keyboard.nextLine();
+    String nameLast = keyboard.nextLine();
+    String ssn = keyboard.nextLine();
 }
