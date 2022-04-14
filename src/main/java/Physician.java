05973add23e1d0c380d6aafe5379b7f45bed4ebd
Branden267 <60358292+Branden267@users.noreply.github.com>
2022-04-13 20:56:40 -0400

    Imported necessary packages

--- src/main/java/Physician.java
+++ src/main/java/Physician.java
@@ -1,24 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Physician extends EmergencyRoom{
     
-    String tests;
-    String observation;
-    String symptoms;
-    String discharge;
-    String medications;
-    String diagnoses;
+    Scanner keyboard = new Scanner(System.in);
     
-    public Physician(String tests, String observation, String symptoms, String discharge,
-    String medications, String diagnoses){
-        
-        super(tests, observation, symptoms, discharge, medications, diagnoses);
-        
-    }
+    //user input to search for patient
+    String nameFirst = keyboard.nextLine();
+    String nameLast = keyboard.nextLine();
+    String ssn = keyboard.nextLine();
     
-    public String toString()
-    {
-        return (super.toString()+"The tests required by the physician: "+ tests +", doctors observation notes: "
-                + observation +", symptoms noticed: "+ symptoms +", discharge instructions: "+ discharge +
-                ", prescribed medications: "+ medications +", final diagnoses: "+ diagnoses);
-    }
-}
+            
 }
