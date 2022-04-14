150d7803d3e1edc2764544ddbf77b56cd0eb4b1e
Branden267 <60358292+Branden267@users.noreply.github.com>
2022-04-13 21:34:56 -0400

    Added variables for Physician

--- src/main/java/Physician.java
+++ src/main/java/Physician.java
@@ -11,7 +11,34 @@ public class Physician extends EmergencyRoom{
     //user input to search for patient
     String nameFirst = keyboard.nextLine();
     String nameLast = keyboard.nextLine();
-    String ssn = keyboard.nextLine();
+    String ssn = keyboard.nextLine();          
     
-            
+    //variables for doctor to input
+    String tests;
+    String observation;
+    String symptoms;
+    String discharge;
+    String medications;
+    String diagnoses;
+    
+    //should be called when sumbit button is pushed by registrar
+    public void updatePatient()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:8888/hospitalSystem", "root", "root");
+                Statement stmt = con.createStatement();
+                //insert patient
+                stmt.executeUpdate("INSERT INTO `hospitalSystem`.`Patient`\n" + "(`patientID`,\n" + "`firstName`,\n" + 
+                        "`lastName`,\n" + "`dob`,\n" + "`address`,\n" + "`zip`,\n" + "`ssn`,\n" + "`insurance`,\n" + 
+                        "`primaryPhys`,\n" + "`height`,\n" + "`weight`,\n" + "`vax1`,\n" + "`vax2`,\n" + "`symptom`,\n" + 
+                        "`allergy`,\n" + "`meds`,\n" + "`alcdrug`)\n" + 
+                        "VALUES\n" + "(AUTO_INCREMENT,\n" + nameFirst + ",\n" + nameLast +",\n" + dob + ",\n" +
+                         address + ",\n" + zip + ",\n" + ssn + ",\n" + insurance + ",\n" + phys + ",\n" + height + ",\n" +
+                         weight + ",\n" + vax1 + ",\n" + vax2 + ",\n" + symptom + ",\n" + allergy + ",\n" +
+                         meds + ",\n" + alcdrug + ");");
+        }catch(ClassNotFoundException | SQLException e){System.out.println(e);}
+    }
+
 }
