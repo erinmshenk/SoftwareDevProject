978ef6deaf6243dd22bbf92a3d7d27a2922f54a4
aircu <aircu@137.49.158.227>
2022-04-20 18:39:17 -0400

    deleted unneeded Scene.fxml

--- SoftwareDevProjectV2/src/main/java/MainApp.java
+++ SoftwareDevProjectV2/src/main/java/MainApp.java
@@ -169,38 +169,6 @@ public class MainApp extends Application {
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println(e);
         }
-
-        Scanner keyboard = new Scanner(System.in);
-
-        String username = keyboard.nextLine();
-        keyboard.nextLine();
-
-        String password = keyboard.nextLine();
-
-        //usernames for all parties that can log in
-        String nuser = "nurse";
-        String ruser = "registrar";
-        String puser = "physician";
-        String buser = "billing";
-
-        //passwords for all parties that can log in
-        String npass = "123!";
-        String rpass = "456!";
-        String ppass = "789!";
-        String bpass = "010!";
-
-        if (username.equals(nuser) && password.equals(npass)) {
-            //call nurse gui
-        } else if (username.equals(ruser) && password.equals(rpass)) {
-            //call registrar gui
-        } else if (username.equals(puser) && password.equals(ppass)) {
-            //call physician gui
-        } else if (username.equals(buser) && password.equals(bpass)) {
-            //call billing gui
-        } else {
-            exit(0);
-        }
-
     }
 
 }
