8184425b3a44b11d9795bed5d79586a2dafdeebb
Erin Shenk <eshenk@cs.hartford.edu>
2022-04-21 18:12:48 -0400

    changes

--- SoftwareDevProjectV2/src/main/java/Registrar.java
+++ SoftwareDevProjectV2/src/main/java/Registrar.java
@@ -13,41 +13,25 @@ import java.sql.Statement;
  *
  * @author erinshenk
  */
-public class Registrar 
+public class Registrar
 {
-        //patient information variables
-    String nameFirst;
-    String nameLast;
-    String dob;
-    String address;
-    String zip;
-    String ssn;
-    String insurance;
-    String phys;
-    int height;
-    int weight;
-    String vax1;
-    String vax2;
-    String symptom;
-    String allergy;
-    String meds;
-    boolean alcdrug;
-    
+  
     //should be called when sumbit button is pushed by registrar
-    public void insertPatient()
+    public void insertPatient(String nameFirst, String nameLast, String dob, String address, String zip, String ssn, String insurance, String phys,
+            int height, int weight, String vax1, String vax2, String symptom, String allergy, String meds, boolean alcdrug)
     {
         System.out.println("it actually printed!!!!!");
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
-                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:8888/hospitalSystem", "root", "root");
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:8889/hospitalSystem", "root", "root");
                 Statement stmt = con.createStatement();
                 //insert patient
-                stmt.executeUpdate("INSERT INTO `hospitalSystem`.`Patient`\n" + "(`patientID`,\n" + "`firstName`,\n" + 
+                stmt.executeUpdate("INSERT INTO `hospitalSystem`.`Patient`\n" + "(" + "`firstName`,\n" + 
                         "`lastName`,\n" + "`dob`,\n" + "`address`,\n" + "`zip`,\n" + "`ssn`,\n" + "`insurance`,\n" + 
                         "`primaryPhys`,\n" + "`height`,\n" + "`weight`,\n" + "`vax1`,\n" + "`vax2`,\n" + "`symptom`,\n" + 
                         "`allergy`,\n" + "`meds`,\n" + "`alcdrug`)\n" + 
-                        "VALUES\n" + "(AUTO_INCREMENT,\n" + nameFirst + ",\n" + nameLast +",\n" + dob + ",\n" +
+                        "VALUES\n" + "(" + nameFirst + ",\n" + nameLast +",\n" + dob + ",\n" +
                          address + ",\n" + zip + ",\n" + ssn + ",\n" + insurance + ",\n" + phys + ",\n" + height + ",\n" +
                          weight + ",\n" + vax1 + ",\n" + vax2 + ",\n" + symptom + ",\n" + allergy + ",\n" +
                          meds + ",\n" + alcdrug + ");");
