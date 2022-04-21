27fee21257455a0e60585b0a6326f1959c93ed03
aircu <aircu@137.49.158.227>
2022-04-21 11:19:12 -0400

    update on fxml

--- SoftwareDevProjectV2/src/main/java/Controllers/NewPatientController.java
+++ SoftwareDevProjectV2/src/main/java/Controllers/NewPatientController.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controllers;
 
-/**
- *
- * @author aircu
- */
-public class NewPatientController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class NewPatientController implements Initializable {
+    private Stage stage;
+    private Scene scene;
+        
+    @FXML
+    private TextField patientIDInput;
+    @FXML
+    private TextField firstNameInput;
+
+
     
+//New Patient Form Controller
+    @FXML
+    private void submitNewForm(ActionEvent event) throws IOException {
+        String patientID = patientIDInput.getText();
+        String fistName = firstNameInput.getText();
+        System.out.println(patientID);
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/NewPatientForm.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
 }
