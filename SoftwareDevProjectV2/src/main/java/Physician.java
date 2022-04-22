3a231ac33bfe5a7c6f0bcbf9da0b5ec8e4769576
Branden267 <60358292+Branden267@users.noreply.github.com>
2022-04-22 15:56:44 -0400

    Update functions to edit patient file

--- SoftwareDevProjectV2/src/main/java/Physician.java
+++ SoftwareDevProjectV2/src/main/java/Physician.java
@@ -1,7 +1,9 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Physician{
@@ -30,11 +32,16 @@ public class Physician{
                 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:8888/hospitalSystem", "root", "root");
                 Statement stmt = con.createStatement();
                 //insert data inputed by Physician
-                stmt.executeUpdate("INSERT INTO `hospitalSystem`.`Patient`\n" + "(`Tests Required`,\n" 
-                        + "`Observations`,\n" + "`Symptoms`,\n" + "`Discharge Information`,\n" 
-                        + "`Prescribed Medications`,\n" + "`Diagnoses`,\n" + 
-                        "VALUES\n" + "(AUTO_INCREMENT,\n" + tests + ",\n" + observation +",\n" + symptoms + 
-                        ",\n" + discharge + ",\n" + medications + ",\n" + diagnoses + ");");
+               PreparedStatement prst = con.prepareStatement("INSERT INTO `hospitalSystem`.`Patient`\n" + "(" + "`tests`,\n" + 
+                        "`observations`,\n" + "`symptoms`,\n" + "`discharge`,\n" + "`medications`,\n" + "`diagnoses`)\n" + 
+                        "VALUES\n" + "(?, ?, ?, ?, ?, ?);");
+                
+                prst.setString(1, tests);
+                prst.setString(2, observations);
+                prst.setString(3, symptoms);
+                prst.setString(4, discharge);
+                prst.setString(5, medications);
+                prst.setString(6, diagnoses);
         }catch(ClassNotFoundException | SQLException e){System.out.println(e);}
     }
 
