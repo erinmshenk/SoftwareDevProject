3fb9d1767c9c52feffa600f879400c771206cef5
Erin Shenk <eshenk@cs.hartford.edu>
2022-04-25 19:41:40 -0400

    constructor

--- SoftwareDevProjectV2/src/main/java/Registrar.java
+++ SoftwareDevProjectV2/src/main/java/Registrar.java
@@ -17,40 +17,65 @@ import java.sql.Statement;
 public class Registrar
 {
   
+    public Registrar(String nameFirst, String nameLast, String dob, String address, String zip, String ssn, String insurance, String phys,
+            int height, int weight, String vax1, String vax2, String symptom, String allergy, String meds, boolean alcdrug)
+    {
+        String nameFirst = nameFirst;
+        String nameLast = nameLast;
+        String dob = dob; 
+        String address = address; 
+        String zip = zip; 
+        String ssn = snn; 
+        String insurance = insurance;
+        String phys = phys;
+        int height = height; 
+        int weight = weight;
+        String vax1 = vax1;
+        String vax2 = vax2;
+        String symptom = symptom;
+        String allergy = allergy; 
+        String meds = meds;
+        boolean alcdrug = alcdrug;            
+    }
     //should be called when sumbit button is pushed by registrar
     public void insertPatient(String nameFirst, String nameLast, String dob, String address, String zip, String ssn, String insurance, String phys,
             int height, int weight, String vax1, String vax2, String symptom, String allergy, String meds, boolean alcdrug)
     {
-//        try
-//        {
-//                Class.forName("com.mysql.jdbc.Driver");
-//                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:8889/hospitalSystem", "root", "root");
-//                Statement stmt = con.createStatement();
-//                //insert patient
-//                
-//                PreparedStatement prst = con.prepareStatement("INSERT INTO `hospitalSystem`.`Patient`\n" + "(" + "`firstName`,\n" + 
-//                        "`lastName`,\n" + "`dob`,\n" + "`address`,\n" + "`zip`,\n" + "`ssn`,\n" + "`insurance`,\n" + 
-//                        "`primaryPhys`,\n" + "`height`,\n" + "`weight`,\n" + "`vax1`,\n" + "`vax2`,\n" + "`symptom`,\n" + 
-//                        "`allergy`,\n" + "`meds`,\n" + "`alcdrug`)\n" + 
-//                        "VALUES\n" + "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
-//                prst.setString(1, nameFirst);
-//                prst.setString(2, nameLast);
-//                prst.setString(3, dob);
-//                prst.setString(4, address);
-//                prst.setString(5, zip);
-//                prst.setString(6, ssn);
-//                prst.setString(7, insurance);
-//                prst.setString(8, phys);
-//                prst.setInt(9, height);
-//                prst.setInt(10, weight);
-//                prst.setString(11, vax1);
-//                prst.setString(12, vax2);
-//                prst.setString(13, symptom);
-//                prst.setString(14, allergy);
-//                prst.setString(15, meds);
-//                prst.setBoolean(16, alcdrug);
-//                
-//                prst.executeUpdate();
-//        }catch(ClassNotFoundException | SQLException e){System.out.println(e);}
+        try
+        {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:8889/hospitalSystem", "root", "root");
+                Statement stmt = con.createStatement();
+                //insert patient
+                
+                PreparedStatement prst = con.prepareStatement("INSERT INTO `hospitalSystem`.`Patient`\n" + "(" + "`firstName`,\n" + 
+                        "`lastName`,\n" + "`dob`,\n" + "`address`,\n" + "`zip`,\n" + "`ssn`,\n" + "`insurance`,\n" + 
+                        "`primaryPhys`,\n" + "`height`,\n" + "`weight`,\n" + "`vax1`,\n" + "`vax2`,\n" + "`symptom`,\n" + 
+                        "`allergy`,\n" + "`meds`,\n" + "`alcdrug`)\n" + 
+                        "VALUES\n" + "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+                prst.setString(1, nameFirst);
+                prst.setString(2, nameLast);
+                prst.setString(3, dob);
+                prst.setString(4, address);
+                prst.setString(5, zip);
+                prst.setString(6, ssn);
+                prst.setString(7, insurance);
+                prst.setString(8, phys);
+                prst.setInt(9, height);
+                prst.setInt(10, weight);
+                prst.setString(11, vax1);
+                prst.setString(12, vax2);
+                prst.setString(13, symptom);
+                prst.setString(14, allergy);
+                prst.setString(15, meds);
+                prst.setBoolean(16, alcdrug);
+                
+                prst.executeUpdate();
+        }catch(ClassNotFoundException | SQLException e){System.out.println(e);}
+    }
+    
+    public void passInfo()
+    {
+        
     }
 }
